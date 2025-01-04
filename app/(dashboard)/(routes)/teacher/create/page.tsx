5832a6ad596240f2ccce402c0e5c2949e63b0810
Yash <randomyashh@gmail.com>
2025-01-04 20:08:31 +0530

    modify course creation api

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -38,8 +38,9 @@ const CourseCreatePage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post(`/api/course`, values);
+      const response = await axios.post(`/api/courses`, values);
       router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course created successfully");
     } catch (error) {
       console.error(error);
       toast.error("Something went wrong");
