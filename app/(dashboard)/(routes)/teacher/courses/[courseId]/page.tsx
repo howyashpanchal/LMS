10b44f4cd61831043de2fea42c4b97156c47535b
Yash <randomyashh@gmail.com>
2025-01-05 21:53:43 +0530

    Update files in app/(dashboard)/(routes)/teacher/courses/[courseId]/

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,66 @@
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
+
+export default async function CourseIDPage({
+  params,
+}: {
+  params: { courseId: string };
+}) {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requireFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requireFields.length; // number of require field
+  const completedFields = requireFields.filter(Boolean).length; // number of completed fields
+
+  const progress = `(${completedFields}/${totalFields})`;
+
+  return (
+    <>
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">Course</h1>
+            <span className="text-sm">Completed all fields {progress}</span>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customise your course</h2>
+            </div>
+            <TitleForm initialData={course} courseId={course.id} />
+            <DescriptionForm initialData={course} courseId={course.id} />
+            <ImageForm initialData={course} courseId={course.id} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
