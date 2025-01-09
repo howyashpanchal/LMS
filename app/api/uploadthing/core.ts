9d5aa473e638ee4285312c95c36b3adc6509e232
Yash <randomyashh@gmail.com>
2025-01-09 12:55:55 +0530

    Fix: Update upload handling in core API for proper metadata access

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,27 +1,56 @@
 import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const handleAuth = async () => {
-    const {userId} = await auth();
-    if (!userId) throw new Error("Unauthorized");
-    return {userId}
-}
+// const handleAuth = async () => {
+//     const {userId} = await auth();
+//     if (!userId) throw new Error("Unauthorized");
+//     return {userId}
+// }
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-    courseImage : f({image: {maxFileSize : "4MB" , maxFileCount : 1}})
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
+    // courseImage : f({image: {maxFileSize : "4MB" , maxFileCount : 1}})
+    // .middleware(async() => await handleAuth())
+    // .onUploadComplete(async ({ metadata }) => {
+    //     console.log("Upload complete for userId:", metadata.userId);
+    // }),
 
-    courseAttachment : f(["text", "image", "pdf", "video", "audio"])
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
+    // courseAttachment: f({
+    //     text: { maxFileSize: "4MB", maxFileCount: 1 },
+    //     image: { maxFileSize: "4MB", maxFileCount: 1 },
+    //     pdf: { maxFileSize: "4MB", maxFileCount: 1 },
+    //     video: { maxFileSize: "16MB", maxFileCount: 1 },
+    //     audio: { maxFileSize: "8MB", maxFileCount: 1 },
+    // })
+    // .middleware(async () => await handleAuth())
+    // .onUploadComplete(async ({ metadata }) => {
+    //     console.log("Upload complete for userId:", metadata.userId);
+    // }),
 
-    chapterVideo : f({video : {maxFileSize : "512GB" , maxFileCount : 1}})
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
+    // chapterVideo : f({video : {maxFileSize : "512GB" , maxFileCount : 1}})
+    // .middleware(() => handleAuth())
+    // .onUploadComplete(async ({ metadata }) => {
+    //     console.log("Upload complete for userId:", metadata.userId);
+    // }),
+    imageUploader : f({
+        image:{
+            maxFileSize : "4MB",
+            maxFileCount : 1
+        },
+    })
+    .middleware(async () => {
+        const {userId} = await auth();
+        if (!userId) throw new UploadThingError("Unauthorized");
+        return {userId};
+    })
+    .onUploadComplete(async ({ metadata }) => {
+        console.log("Uploaded by user ID:", metadata.userId);
+        
+        return { uploadedBy: metadata.userId };
+    })
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
