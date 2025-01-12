61400bebdc05b61356f8170007834ba09b19cef8
Yash <randomyashh@gmail.com>
2025-01-12 16:15:04 +0530

    Update chapter page in app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx to improve chapter details display and functionality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,11 +1,16 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 import { ChapterTitleForm } from "../_components/chapter-title-form";
+import { ChapterDescriptionForm } from "../_components/chapter-description-form";
+import { ChapterAccessForm } from "../_components/chapter-access-form copy";
+import { ChapterVideoForm } from "../_components/chapter-video-form";
+import { Banner } from "@/components/banner";
+import { ChapterAction } from "../_components/chapter-action";
 
 const ChapterIdPage = async ({
   params,
@@ -17,10 +22,12 @@ const ChapterIdPage = async ({
     return redirect("/");
   }
 
+  const { chapterId, courseId } = await params;
+
   const chapter = await db.chapter.findUnique({
     where: {
-      id: params.chapterId,
-      courseId: params.courseId,
+      id: chapterId,
+      courseId: courseId,
     },
     include: {
       muxData: true,
@@ -36,8 +43,16 @@ const ChapterIdPage = async ({
 
   const completionText = `(${completedFields}/${totalFields})`;
 
+  const isComplete = requiredFields.every(Boolean);
+
   return (
     <>
+      {!chapter.isPublished && (
+        <Banner
+          variant="warning"
+          label="This Chapter is Un-published. It'll not be visible in the course!"
+        />
+      )}
       <div className="p-6">
         <div className="flex items-center justify-between">
           <div className="w-full">
@@ -55,6 +70,12 @@ const ChapterIdPage = async ({
                   Completed all fields {completionText}
                 </span>
               </div>
+              <ChapterAction
+                disabled={!isComplete}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                isPublished={chapter.isPublished}
+              />
             </div>
           </div>
         </div>
@@ -70,7 +91,32 @@ const ChapterIdPage = async ({
                 courseId={params.courseId}
                 chapterId={params.chapterId}
               />
+              <ChapterDescriptionForm
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
             </div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl">Access Setting</h2>
+            </div>
+            <ChapterAccessForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Video} />
+              <h2 className="text-xl">Add a video</h2>
+            </div>
+            <ChapterVideoForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
           </div>
         </div>
       </div>
