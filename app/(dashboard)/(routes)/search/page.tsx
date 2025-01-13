741e5a6929d3856e56ddbd221414c87a12ed5328
Yash <randomyashh@gmail.com>
2025-01-13 22:48:11 +0530

    Update search page in app/(dashboard)/(routes)/search/page.tsx to enhance search functionality and user experience

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,45 @@
+import { db } from "@/lib/db";
 import React from "react";
+import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/SearchInput";
+import { getCourses } from "@/actions/get-courses";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { CoursesList } from "@/components/courses-list";
 
-export default function SearchPage() {
-  return <div>This is a Search page lorem50</div>;
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
 }
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+        <CoursesList />
+      </div>
+    </>
+  );
+};
+export default SearchPage;
