400139db2be2edde4ca8a49db19b7d87cfcccdca
Yash <randomyashh@gmail.com>
2025-02-14 11:00:38 +0530

    Update CoursesList component to improve course listing and grid layout functionality

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -14,17 +14,19 @@ export const CoursesList = ({ items }: CourseListPageProps) => {
     <>
       <div>
         <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
-          <CourseCard
-            key={items.id}
-            id={items.id}
-            title={items.title}
-            imageUrl={items.imageUrl}
-            chapterLength={items.chapter.length}
-            price={items.price}
-            progress={items.progress}
-            category={items?.category?.name}
-          />
-          ) : (
+          {items?.map((item) => (
+            <CourseCard
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              imageUrl={item.imageUrl!}
+              chapterLength={item.chapter.length}
+              price={item.price!}
+              progress={item.progress}
+              category={item?.category?.name!}
+            />
+          ))}
+          {items?.length === 0 && (
             <div className="text-center text-sm text-muted-foreground mt-10">
               No Courses Found
             </div>
